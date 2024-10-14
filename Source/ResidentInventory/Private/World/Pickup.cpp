d282586d53b70e7466df0979d0e3ec16ba939e56
Srujan Lokhande <srujanlokhande72@gmail.com>
2024-10-14 05:49:58 +0530

    Added Pickup base class with all functionalities and added test items in the data table

---
+++ Source/ResidentInventory/Private/World/Pickup.cpp
@@ -0,0 +1,158 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World/Pickup.h"
+
+#include "Items/ItemBase.h"
+
+
+// Sets default values
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	PickupStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickUp Actor Mesh");
+	PickupStaticMesh->SetSimulatePhysics(true);
+	SetRootComponent(PickupStaticMesh);
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitializePickup(UItemBase::StaticClass(), PickupItemQuantity);
+}
+
+void APickup::InitializePickup(const TSubclassOf<UItemBase> BaseClass, int32 ItemQuantity)
+{
+	if(ItemDataTable && !DesiredID.IsNone())
+	{
+		//struct from the data Table
+		const FItemData* ItemDataRow = ItemDataTable->FindRow<FItemData>(DesiredID, DesiredID.ToString());
+
+		ItemReference = NewObject<UItemBase>(this, BaseClass);
+		
+		ItemReference->ID = ItemDataRow->ID;
+		ItemReference->ItemType = ItemDataRow->ItemType;
+		ItemReference->ItemRarity = ItemDataRow->ItemRarity;
+		ItemReference->NumericData = ItemDataRow->NumericData;
+		ItemReference->TextData = ItemDataRow->TextData;
+		ItemReference->AssetData = ItemDataRow->AssetData;
+		
+		ItemQuantity <=0 ? ItemReference->SetItemQuantity(1) : ItemReference->SetItemQuantity(ItemQuantity);
+
+		PickupStaticMesh->SetStaticMesh(ItemDataRow->AssetData.ItemMesh);
+
+		UpdateInteractableData();
+	}
+}
+
+void APickup::UpdateInteractableData()
+{
+	InstanceInteractableData.InteractableType = EInteractableType::Pickup;
+	InstanceInteractableData.Action = ItemReference->TextData.InteractionText;
+	InstanceInteractableData.Name = ItemReference->TextData.Name;
+	InstanceInteractableData.Quantity = ItemReference->ItemQuantity;
+	InteractableData = InstanceInteractableData;
+}
+
+void APickup::InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity)
+{
+	ItemReference =ItemToDrop;	
+	PickupItemQuantity <=0 ? ItemReference->SetItemQuantity(1) : ItemReference->SetItemQuantity(InQuantity);
+	ItemReference->NumericData.ItemWeight = ItemToDrop->GetItemSingleWeight();
+	PickupStaticMesh->SetStaticMesh(ItemToDrop->AssetData.ItemMesh);
+
+	UpdateInteractableData();
+}
+
+void APickup::BeginFocus()
+{
+	if(PickupStaticMesh)
+	{
+		// this creates a layer on the top of the mesh and applies the material
+		PickupStaticMesh->SetRenderCustomDepth(true);
+	}	
+}
+
+void APickup::EndFocus()
+{
+	if(PickupStaticMesh)
+	{
+		PickupStaticMesh->SetRenderCustomDepth(false);
+	}
+}
+
+void APickup::Interact(AResidentInventoryCharacter* PlayerCharacter)
+{
+	if(PlayerCharacter)
+	{
+		TakePickup(PlayerCharacter);
+	}
+}
+
+
+void APickup::TakePickup(const AResidentInventoryCharacter* PickupTaker)
+{
+	// checks if this actor is on the way for destruction because after the player picks it up
+	// we will destroy the actor from the world, so if the player spams the equip Key
+	// it won't run this function again if the item is already picked up
+	if(!IsPendingKillPending())
+	{
+		if(ItemReference)
+		{
+			// if(UInventoryComponent* PlayerInventory = PickupTaker->GetInventory())
+			// {
+			// 	const FItemAddResult AddResult = PlayerInventory->HandleAddItem(ItemReference);
+			//
+			// 	switch (AddResult.OperationResult) {
+			// 	case EItemAddResult::IAR_NoItemAdded:
+			// 		break;
+			// 	case EItemAddResult::IAR_PartialAmountItemAdded:
+			// 		UpdateInteractableData();
+			// 		PickupTaker->UpdateInteractionWidget();
+			// 		break;
+			// 	case EItemAddResult::IAR_AllItemAdded:
+			// 		Destroy();
+			// 		break;
+			// 	default: ;
+			// 	}
+			// 	UE_LOG(LogTemp, Warning, TEXT("%s"), *AddResult.ResultMessage.ToString());
+			// }
+			// else
+			// {
+			// 	UE_LOG(LogTemp, Warning, TEXT("Inventory Component Null"));
+			// }
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Pickup Item Reference is Null"));
+		}
+	}
+}
+
+void APickup::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName ChangedPropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.GetPropertyName() : NAME_None;
+	
+	if(ChangedPropertyName == GET_MEMBER_NAME_CHECKED(APickup, DesiredID))
+	{
+		if(ItemDataTable)
+		{
+			if(const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredID, DesiredID.ToString()))
+			{
+				PickupStaticMesh->SetStaticMesh(ItemData->AssetData.ItemMesh);
+			}
+			
+		}
+	}
+}
+
+
+
+
+
+
---
+++ Source/ResidentInventory/Public/World/Pickup.h
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/InteractionInterface.h"
+#include "Pickup.generated.h"
+
+
+struct FInteractableData;
+struct FItemData;
+class UItemBase;
+class UStaticMeshComponent;
+class UDataTable;
+
+
+UCLASS()
+class RESIDENTINVENTORY_API APickup : public AActor, public IInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APickup();
+
+	// take the items from the data table during begin play
+	void InitializePickup(const TSubclassOf<UItemBase> BaseClass, int32 ItemQuantity);
+
+	// to initialize the items from the data table to be ready for dropping
+	void InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity);	
+
+	// getter for the Item
+	FORCEINLINE UItemBase* GetItemData() const { return ItemReference;}
+
+	virtual void BeginFocus() override;
+	virtual void EndFocus() override;
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Pickup | Components")
+	UStaticMeshComponent* PickupStaticMesh;	
+
+	// The data table with all the info we give to a specific actor
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Database")
+	UDataTable* ItemDataTable;
+
+	// the ID of the item which can be referenced with the ItemID in the data table to get a specific item
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Database")
+	FName DesiredID;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pickup | Item Reference")
+	UItemBase* ItemReference;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Initialization")
+	int32 PickupItemQuantity;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Pickup | Interaction")
+	FInteractableData InstanceInteractableData;
+	
+	virtual void BeginPlay() override;
+
+	virtual void Interact(AResidentInventoryCharacter* PlayerCharacter) override;
+	void UpdateInteractableData();
+	void TakePickup(const AResidentInventoryCharacter* PickupTaker);
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;	
+#endif
+};
